8c4cb53cadd543ac867bd56623174b8e992f06d6
elegantsignal <elegantsignal@gmail.com>
2020-04-25 11:45:58 +0300

    1

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"workshop/internal/handler"
+
+	"github.com/go-chi/chi"
+)
+
 func main() {
 
+	h := handler.NewHandler()
+
+	r := chi.NewRouter()
+
+	r.Get("/hello", h.Hello)
+
+	err := http.ListenAndServe(":8080", r)
+	log.Fatal(err)
+
+	log.Println("shutting server down")
 }
